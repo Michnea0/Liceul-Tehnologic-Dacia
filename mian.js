5f90c734101b433cd5c7a5efae504ace4ecfe22b
Dollzzy <greenfox.ro@gmail.com>
2024-10-26 01:38:03 +0100

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,28 @@
+let SelectorA,SelectorB,zoneONE,zoneTWO
+          
+          SelectorA = document.getElementById("SelectorA")
+          SelectorB = document.getElementById("SelectorB")
+          zoneONE = document.querySelector(".about-us")
+          zoneTWO = document.querySelector(".projects")
+
+          SelectorA.addEventListener("click", ()=>
+          {
+                zoneONE.style.display = "block"
+                zoneTWO.style.display = "none"    
+                SelectorA.style.borderBottom = "1px solid grey"
+                SelectorA.style.color = "black"
+                SelectorB.style.borderBottom = "none"
+                SelectorB.style.color = "grey"
+          })
+
+          SelectorB.addEventListener("click", ()=>
+          {
+                    zoneONE.style.display = "none"
+                    zoneTWO.style.display = "block" 
+                    SelectorB.style.borderBottom = "1px solid grey"
+                    SelectorB.style.color = "black"   
+                    SelectorA.style.borderBottom = "none"
+                    SelectorA.style.color = "grey"
+          })
+
+          
